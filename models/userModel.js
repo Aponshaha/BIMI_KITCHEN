0401ccd29750aa545f256eb53fa410cd681d2a42
Aponshaha <devapon77@gmail.com>
2022-01-07 10:56:46 +0900

    1st commit

---
+++ models/userModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const userSchema = mongoose.Schema({
+    name: {type: 'string', require},
+    email: {type: 'string', require},
+    password: {type: 'string', require},
+    isAdmin: {type: 'Boolean', require, default: false},
+},{
+    timestamps : true,
+})
+
+module.exports = mongoose.model('users', userSchema)
