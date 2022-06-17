d0e00b44aecad06117afd8a906bdb7435e1aa8d7
Rohul Amin <rohul.amin@bongobd.com>
2022-06-18 00:46:00 +0600

    Checkout success and failed page update

--- client/src/components/CheckoutSuccess.js
+++ client/src/components/CheckoutSuccess.js
@@ -1,5 +1,16 @@
+import './CheckoutSuccess.css'
 const CheckoutSuccess = () => {
-    return ( <h2>Checkout Successfully</h2> );
+    return ( 
+    <div className="root">
+      <div className="card">
+      <div style={{'border-radius':'200px', 'height':'200px', 'width':'200px', background: '#F8FAF5',margin:'0 auto'}}>
+        <i className="checkmark">✓</i>
+      </div>
+        <h1>Success</h1> 
+        <p>We received your purchase request;<br/> we'll be in touch shortly!</p>
+      </div>
+    </div>
+    );
 }
  
 export default CheckoutSuccess;
